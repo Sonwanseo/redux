b63f18e442fb5b39408cf99add5a0acd947f123d
Sonwanseo <sonwanseo@gmail.com>
2020-06-22 09:23:28 +0900

    #3.1 Connecting the Store/make Store.js

--- vanilla-redux/src/index.js
+++ vanilla-redux/src/index.js
@@ -2,4 +2,9 @@ import React from "react";
 import ReactDom from "react-dom";
 import App from "./components/App";
 
-ReactDom.render(<App />, document.getElementById("root"));
+ReactDom.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById("root")
+);
